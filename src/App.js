5f43cba0db359e29983dfbc4364d9e82e44adfd6
agatta1504 <agatta1504@gmail.com>
2017-03-18 09:32:14 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clicks: 0,
+      show: true
+    }
+  }
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 })
+  }
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 })
+  }
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={ this.IncrementItem }>Click to increment by 1</button>
+        <button onClick={ this.DecreaseItem }>Click to decrease by 1</button>
+        <button onClick={ this.ToggleClick }>
+          { this.state.show ? 'Hide number' : 'Show number' }
+        </button>
+        { this.state.show ?  <h2>{ this.state.clicks }</h2> : '' }
+      </div>
+    );
+  }
+}
+
+export default App;
